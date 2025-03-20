13422e1f882f8f775e6e58de6ae655eb6fe90dfd
daffaputra09 <daffaputra863@gmail.com>
2025-03-20 13:56:11 +0700

    feat: Praktikum 1 Insertion Sort

--- Sorting06.java
+++ Sorting06.java
@@ -44,6 +44,18 @@ public class Sorting06 {
             data[min] = temp;
         }
     }
+
+    void insertionSort(){
+        for (int i = 1; i < jumData-1; i++){
+            int temp = data[i];
+            int j = i-1;
+            while (j >= 0 && data[j] > temp){
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j+1] = temp;
+        }
+    }
 }
 
 
--- SotingMain06.java
+++ SotingMain06.java
@@ -2,6 +2,7 @@ public class SotingMain06 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] = {30, 10, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
 
         Sorting06 dataUrut1 = new Sorting06(a, a.length);
         System.out.println("Data awal 1");
@@ -16,5 +17,11 @@ public class SotingMain06 {
         dataUrut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataUrut2.tampil();
+
+        Sorting06 dataUrut3 = new Sorting06(c, c.length);
+        System.out.println("Data awal 3");
+        dataUrut3.tampil();
+        dataUrut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
     }
 }
